78229a3158d1b0094cf2b94b8fb58d0fc6ed546b
Moritz Tim W. <90388353+moritztim@users.noreply.github.com>
2024-07-03 14:23:53 +0200

    add `astro-electron` and `electron-forge`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from "astro/config";
-
+import electron from "astro-electron";
 import svelte from "@astrojs/svelte";
 import preprocess from "svelte-preprocess";
 
 export default defineConfig({
-  site: "https://pianolatron.stanford.edu/",
-  base: "/",
-  trailingSlash: "always",
-  server: { port: 8080 },
-  integrations: [
-    svelte({
-      preprocess: [
-        preprocess.scss({
-          prependData: `@use "sass:math";@use 'src/styles/sass-globals.scss' as *;`,
-        }),
-      ],
-    }),
-  ],
+	site: "https://pianolatron.stanford.edu/",
+	base: "/",
+	trailingSlash: "always",
+	server: { port: 8080 },
+	integrations: [
+		svelte({
+			preprocess: [
+				preprocess.scss({
+					prependData: `@use "sass:math";@use 'src/styles/sass-globals.scss' as *;`,
+				}),
+			],
+		}),
+		electron()
+	],
 });
